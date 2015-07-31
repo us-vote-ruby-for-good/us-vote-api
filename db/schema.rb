4968107ecd923f57fda123d385896cf6599defa0
Betsy Haibel <betsy.haibel@gmail.com>
2015-07-31 15:52:45 -0400

    state model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731152720) do
+ActiveRecord::Schema.define(version: 20150731194948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "states", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.integer  "drupal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
