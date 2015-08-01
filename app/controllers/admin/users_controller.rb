5ff8acd2ca59a0d816be69968a6f24bca6e4b82b
Tim Morton <tim@timothymorton.com>
2015-07-31 21:17:32 -0400

    Basic user creation

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,23 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.all
   end
+
+  def create
+    generated_password = Devise.friendly_token.first(8)
+    user = User.new(user_params.merge(password: generated_password))
+    if user.save
+      # TODO: send an email to the new user
+      flash[:notice] = "We've sent an email to #{user.email} with login instructions."
+    else
+      flash[:error] = user.errors.full_messages.first
+    end
+    redirect_to action: :index
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
 end
--- spec/features/admin_users_spec.rb
+++ spec/features/admin_users_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 RSpec.feature "User management by admins", type: :feature do
 
+  let(:login_user) { FactoryGirl.create(:user) }
+
   before(:each) do
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
+    login_as(login_user, :scope => :user)
   end
 
   scenario "Creates a new admin user" do
+    new_user_email = 'foo@bar.com'
+
     visit 'admin/users'
-    expect(page).to have_text("Foo")
+    expect(page).to have_text(login_user.email)
+    fill_in 'user_email', with: new_user_email
+    click_button 'Create User'
+    expect(page).to have_text("We've sent an email") # flash message
+    expect(page).to have_text(new_user_email)
   end
 end
